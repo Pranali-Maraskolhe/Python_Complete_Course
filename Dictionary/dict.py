998410f633b1764405ce09a535fa6d88d2e2a401
Pranali-Maraskolhe <pranalimaraskolhe6@gmail.com>
2024-09-20 00:00:06 +0530

    Dictionary Program

---
+++ Dictionary/dict.py
@@ -0,0 +1,15 @@
+marks = {
+    "Harry": 100,
+    "Pranali":200,
+    "Nishant":800 
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# marks.update({"Harry": 99, "Renuka": 100})
+# print(marks)
+
+print(marks.get("Pranali2")) # Prints None
+print(marks["Pranali2"])  # Returns an error
