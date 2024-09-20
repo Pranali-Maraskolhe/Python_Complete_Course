6aabd301e05c8af82b63fff324c8305fb4de197a
Pranali-Maraskolhe <pranalimaraskolhe6@gmail.com>
2024-09-20 20:33:24 +0530

    Average using function

---
+++ functions_and_recursions/avg.py
@@ -0,0 +1,14 @@
+
+# Function Definition
+def avg():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    c = int(input("Enter a number: "))
+
+    res = (a+b+c)/3
+    print("Average is: ", res)
+
+# Function Call
+avg()
+avg()
+avg()
