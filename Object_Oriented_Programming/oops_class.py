a5b052665619a5e4dafb471cdf8ae6362198ff2d
Pranali-Maraskolhe <pranalimaraskolhe6@gmail.com>
2024-09-23 19:07:46 +0530

    use of class

---
+++ Object_Oriented_Programming/oops_class.py
@@ -0,0 +1,12 @@
+class Employee:
+    
+    # Below are the class attribute
+    Profile = "Software Engineer" 
+    Salary = 120000000
+
+
+p1 = Employee()
+p1.name = "Pranali" #   This is an instances attribute
+print(p1.name)
+print(p1.Profile)
+print(p1.Salary)
