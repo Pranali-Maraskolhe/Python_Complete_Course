500ec3db952a0e20cbe0ead41e5f05e5f50356a5
Pranali-Maraskolhe <pranalimaraskolhe6@gmail.com>
2024-09-27 15:48:16 +0530

    In this file if name == main then we can't import the variable and function of that file o module

---
+++ Special_features_python/module.py
@@ -0,0 +1,11 @@
+def myfun():
+    print("Hello World")
+
+
+
+
+if __name__ == "__main__":
+    # If this code is directly executed by running the file its present in
+    print("we are directly running this code")
+    myfun()
+    print(__name__)
