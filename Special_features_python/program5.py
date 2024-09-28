4da04089e4cc558f1a50ec403ba627f4f1f1442e
Pranali-Maraskolhe <pranalimaraskolhe6@gmail.com>
2024-09-28 13:11:14 +0530

    use append mode to add other table in existing file

--- Special_features_python/program5.py
+++ Special_features_python/program5.py
@@ -2,6 +2,12 @@
 
 
 num = int(input("Enter a Number: "))
-with open("multipy.txt", "w") as f:
-    for i in range(1, 11):
-        f.write(f"{i} X {num} = {num*i} \n")
+
+# with open("multipy.txt", "w") as f:
+#     for i in range(1, 11):
+#         f.write(f"{i} X {num} = {num*i} \n")
+
+
+table = [num * i for i in range(1, 11)]
+with open("multipy.txt", "a") as f:
+    f.write(f"Table of {num}: {str(table)} \n")
